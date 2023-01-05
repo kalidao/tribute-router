3deffbac5e1bcfef911fbae988e04c7c57c4b200
z0r0z <92001561+z0r0z@users.noreply.github.com>
2023-01-05 15:31:36 +0530

    ✍️  Add event hook to 1155 receiver.
    ✍️  Add event hook to 1155 receiver.

--- src/TributeRouter.sol
+++ src/TributeRouter.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.4;
 
 import {ERC1155STF} from "@kali/utils/ERC1155STF.sol";
 import {KeepTokenMint} from "./utils/KeepTokenMint.sol";
-import {ERC1155TokenReceiver} from "@keep/KeepToken.sol";
 import {SelfPermit} from "@solbase/src/utils/SelfPermit.sol";
 import {ReentrancyGuard} from "@solbase/src/utils/ReentrancyGuard.sol";
 import {SafeMulticallable} from "@solbase/src/utils/SafeMulticallable.sol";
@@ -697,5 +696,23 @@ contract TributeRouter is
 
             if (!valid) revert InvalidSig();
         }
+    }
+}
+
+/// @notice ERC1155 interface to receive single tokens with hook to emit data.
+/// @author Modified from Solbase (https://github.com/Sol-DAO/solbase/blob/main/src/tokens/ERC1155/ERC1155.sol)
+abstract contract ERC1155TokenReceiver {
+    event DataReceived(bytes data);
+
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata data
+    ) public payable virtual returns (bytes4) {
+        if (data.length != 0) emit DataReceived(data);
+
+        return this.onERC1155Received.selector;
     }
 }
